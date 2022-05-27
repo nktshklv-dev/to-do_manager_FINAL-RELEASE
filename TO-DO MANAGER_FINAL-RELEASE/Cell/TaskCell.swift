d425434aecdfe6455b943c0de126445d68bf47c3
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-05-27 21:46:36 +0300

    created tableview

---
+++ TO-DO MANAGER_FINAL-RELEASE/Cell/TaskCell.swift
@@ -0,0 +1,14 @@
+//
+//  TaskCell.swift
+//  TO-DO MANAGER_FINAL-RELEASE
+//
+//  Created by Nikita  on 5/27/22.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+
+    @IBOutlet var taskIcon: UILabel!
+    @IBOutlet var taskTitle: UILabel!
+}
