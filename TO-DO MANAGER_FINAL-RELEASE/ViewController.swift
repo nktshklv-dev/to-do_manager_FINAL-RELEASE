7976e1e9941f13d231f7728f5be0862c0f7ea5cb
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-05-27 18:58:35 +0300

    Initial Commit

---
+++ TO-DO MANAGER_FINAL-RELEASE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TO-DO MANAGER_FINAL-RELEASE
+//
+//  Created by Nikita  on 5/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
